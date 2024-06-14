82a49108691d22a78d2408333523da2ddef9a508
joaoxmb <joaovictorcgdossantos@gmail.com>
2024-06-14 19:45:38 -0300

    Feat: created hero section

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
-const gulp = require("gulp")
-const sass = require("gulp-sass")(require("sass"))
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const replace = require("gulp-replace");
 
 function compileSass() {
-  return gulp.src("./src/styles/*.scss")
+  return gulp.src("./src/styles/base.scss")
     .pipe(sass({
       outputStyle: "compressed"
     }))
+    .pipe(replace("../", "../../src/"))
     .pipe(gulp.dest("./dist/styles"))
 }
 
+function compileIndex() {
+  return gulp.src("./src/index.html")
+    .pipe(replace("./images", "../src/images"))
+    .pipe(gulp.dest("./dist"))
+}
 
-exports.sass = () => {
+exports.sass = compileSass;
+exports.index = compileIndex;
+
+exports.build = gulp.parallel(compileSass, compileIndex);
+exports.watch = () => {
+  compileIndex();
+  gulp.watch("./src/index.html", { ignoreInitial: false }, gulp.parallel(compileIndex));
   gulp.watch("./src/styles/*.scss", { ignoreInitial: false }, gulp.parallel(compileSass))
 }
